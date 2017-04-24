7496d708d0d66e0058731cdc3b77566bd83b5f01
Ramadani <email.ramadani@gmail.com>
2017-04-24 13:00:25 +0700

    implement costume callback movie response

--- app/src/main/java/id/ramadani/pilem/presenter/TopRatedMoviePresenter.java
+++ app/src/main/java/id/ramadani/pilem/presenter/TopRatedMoviePresenter.java
@@ -1,13 +1,6 @@
 package id.ramadani.pilem.presenter;
 
-import java.util.ArrayList;
-
-import id.ramadani.pilem.api.response.MovieCollectionResponse;
-import id.ramadani.pilem.model.Movie;
 import id.ramadani.pilem.view.MovieView;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by dani on 4/24/17.
@@ -22,27 +15,7 @@ public class TopRatedMoviePresenter extends MoviePresenter implements MoviePrese
 
     @Override
     public void load(int page) {
-        final ArrayList<Movie> movies = new ArrayList<>();
-
         view.showProgress();
-        mService.topRatedList(page).enqueue(new Callback<MovieCollectionResponse>() {
-            @Override
-            public void onResponse(Call<MovieCollectionResponse> call,
-                                   Response<MovieCollectionResponse> response) {
-                MovieCollectionResponse collectionResponse = response.body();
-
-                for (Movie movie: collectionResponse.getMovies()) {
-                    movies.add(movie);
-                }
-
-                view.pushToMovieList(movies);
-                view.hideProgress();
-            }
-
-            @Override
-            public void onFailure(Call<MovieCollectionResponse> call, Throwable t) {
-                view.hideProgress();
-            }
-        });
+        mService.topRated(page).enqueue(new MovieListCallback(view));
     }
 }
--- app/src/main/java/id/ramadani/pilem/presenter/UpcomingMoviePresenter.java
+++
@@ -1,8 +0,0 @@
-package id.ramadani.pilem.presenter;
-
-/**
- * Created by dani on 4/24/17.
- */
-
-public class UpcomingMoviePresenter extends MoviePresenter implements MoviePresenterContract {
-}
